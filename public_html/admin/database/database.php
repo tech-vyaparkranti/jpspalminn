15e479c15dfd9f32fa1a8d2fe8dd9959b17d77c1
vk-mithlesh <mithlesh.jha@vyaparkranti.in>
2025-05-09 13:02:32 +0530

    changes

--- public_html/admin/database/database.php
+++ public_html/admin/database/database.php
@@ -1,7 +1,7 @@
 <?php
 /*user pass db*/
 // $link=mysqli_connect("localhost","u658479103_jps","root","");
-$link = mysqli_connect("localhost", "root", "", "u658479103_jps"); // 'jps' is your local database name
+$link = mysqli_connect("localhost", "jpspalminn", "xH#2a9aTCnr3xd%m", "u658479103_jps"); // 'jps' is your local database name
 
 
 if ($link -> connect_errno) {
